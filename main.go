9016923871d419cac68eb637cd99c2b5055e027d
marutanm <marutanm@users.noreply.github.com>
2016-10-17 23:19:45 +0900

    add middle point

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	table := tablewriter.NewWriter(os.Stdout)
-	table.SetHeader([]string{"min", "max", "release"})
+	table.SetHeader([]string{"min", "50%", "75%", "max", "release"})
 	table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
 
 	const layout = "2006-01-02"
@@ -18,9 +18,13 @@ func main() {
 		estimate := base.AddDate(0, index, -1)
 		term := estimate.Sub(base)
 		maxTerm := base.Add(time.Duration(term.Hours()*1.5) * time.Hour)
+		fifty := estimate.Add(time.Duration(maxTerm.Sub(estimate).Hours()*0.4) * time.Hour)
+		seventyFive := fifty.Add(time.Duration(maxTerm.Sub(estimate).Hours()*0.12) * time.Hour)
 		release := maxTerm.AddDate(0, 5, 0)
 		table.Append([]string{
 			estimate.Format(layout),
+			fifty.Format(layout),
+			seventyFive.Format(layout),
 			maxTerm.Format(layout),
 			release.Format(layout),
 		})
