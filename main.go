07fe0c23bde78227929784d693f63cb8ec2aca90
marutanm <marutanm@users.noreply.github.com>
2016-10-17 23:05:43 +0900

    use tablewriter

--- main.go
+++ main.go
@@ -1,23 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"os"
 	"time"
+
+	"github.com/olekukonko/tablewriter"
 )
 
 func main() {
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"min", "max", "release"})
+
 	const layout = "2006-01-02"
-
 	base := time.Date(2016, 11, 1, 0, 0, 0, 0, time.Local)
-
 	for index := 2; index < 8; index++ {
 		estimate := base.AddDate(0, index, -1)
-		fmt.Println(estimate.Format(layout))
-
 		term := estimate.Sub(base)
-		fmt.Println(term)
-
 		maxTerm := base.Add(time.Duration(term.Hours()*1.5) * time.Hour)
-		fmt.Println("max", maxTerm.Format(layout))
+		release := maxTerm.AddDate(0, 5, 0)
+		table.Append([]string{
+			estimate.Format(layout),
+			maxTerm.Format(layout),
+			release.Format(layout),
+		})
 	}
+	table.Render()
 }
