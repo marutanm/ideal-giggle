f73a0709db8d81610be8ddba96195cae2e8d0efc
marutanm <marutanm@users.noreply.github.com>
2016-10-17 22:31:40 +0900

    multi pattern

--- main.go
+++ main.go
@@ -9,5 +9,9 @@ func main() {
 	const layout = "2006-01-02"
 
 	base := time.Date(2016, 11, 1, 0, 0, 0, 0, time.Local)
-	fmt.Println(base.Format(layout))
+
+	for index := 2; index < 8; index++ {
+		estimate := base.AddDate(0, index, -1)
+		fmt.Println(estimate.Format(layout))
+	}
 }
